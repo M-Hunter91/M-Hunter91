4a9fce3f05d867e0f61c15a65663e77be70bfd6b
M-Hunter91 <121075278+M-Hunter91@users.noreply.github.com>
2025-08-29 05:04:14 +0530

    Add files via upload

--- Pancakes/script.js
+++ Pancakes/script.js
@@ -3,8 +3,20 @@ let multiplier = 0;
 function main() {
     multiplier = document.getElementById("Quantity").value
     let ingredient_1 = document.getElementById("ingredient_1")
+    let ingredient_2 = document.getElementById("ingredient_2")
+    let ingredient_3 = document.getElementById("ingredient_3")
+    let ingredient_4 = document.getElementById("ingredient_4")
+    let ingredient_5 = document.getElementById("ingredient_5")
+    let ingredient_6 = document.getElementById("ingredient_6")
+    let ingredient_7 = document.getElementById("ingredient_7")
 
-    ingredient_1.innerText = multiplier * 5 //add quantity here
+    ingredient_1.innerText = multiplier * 1.5 //add quantity here
+    ingredient_2.innerText = multiplier * 3.5
+    ingredient_3.innerText = multiplier * 1
+    ingredient_4.innerText = multiplier * 0.25
+    ingredient_5.innerText = multiplier * 1.25
+    ingredient_6.innerText = multiplier * 3
+    ingredient_7.innerText = multiplier * 1
 }
 window.onload = function() {
     main()
